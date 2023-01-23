2b0ec5165d3d027e141058f1cba02e661fad6340
Alex Rohregger <alex.rohregger@picuscapital.onmicrosoft.com>
2023-01-23 21:26:41 +0100

    added new chat agents

--- main.py
+++ main.py
@@ -4,19 +4,13 @@ from streamlit_chat import message
 
 from langchain.chains import ConversationChain
 from langchain.llms import OpenAI
+from chains.chat_chain import *
 
-
-def load_chain():
-    """Logic for loading the chain you want to use should go here."""
-    llm = OpenAI(temperature=0)
-    chain = ConversationChain(llm=llm)
-    return chain
-
-chain = load_chain()
+chain = assistant_chain()
 
 # From here down is all the StreamLit UI.
-st.set_page_config(page_title="LangChain Demo", page_icon=":robot:")
-st.header("LangChain Demo")
+st.set_page_config(page_title="Picus ChatGPT", page_icon=":robot:")
+st.header("Picus ChatGPT")
 
 if "generated" not in st.session_state:
     st.session_state["generated"] = []
@@ -26,14 +20,14 @@ if "past" not in st.session_state:
 
 
 def get_text():
-    input_text = st.text_input("You: ", "Hello, how are you?", key="input")
+    input_text = st.text_input("You: ", "What date is it today?", key="input")
     return input_text
 
 
 user_input = get_text()
 
 if user_input:
-    output = chain.run(input=user_input)
+    output = chain.predict(input=user_input)
 
     st.session_state.past.append(user_input)
     st.session_state.generated.append(output)
